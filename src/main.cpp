107f6a65c5be45e1f0185d681c5e79907358ef16
modellfan <febaschu@aol.com>
2025-01-31 17:13:36 +0100

    Two mcp2515 working

--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,14 @@ static const byte MCP2515_MISO = 13;
 static const byte MCP2515_CS = 10;
 static const byte MCP2515_INT = 9;
 
+static const byte MCP2515_CS_CAN2 = 7;
+static const byte MCP2515_INT_CAN2 = 8;
+
 static const uint32_t SPI_CLOCK = 10 * 1000 * 1000;
 static const uint32_t QUARTZ_FREQUENCY = 8UL * 1000UL * 1000UL;
 
 ACAN2515 can(MCP2515_CS, SPI, MCP2515_INT);
+ACAN2515 can2(MCP2515_CS_CAN2, SPI, MCP2515_INT_CAN2);
 
 // ——————————————————————————————————————————————————————————————————————————————
 //  RGB LED Config (ESP32-S3 Built-in)
@@ -87,7 +91,7 @@ void setup()
     SPI.begin(MCP2515_SCK, MCP2515_MISO, MCP2515_MOSI);
 
     // Configure ACAN2515
-    Serial.println("Configuring ACAN2515...");
+    Serial.println("Configuring ACAN2515 CAN 1...");
     ACAN2515Settings settings(QUARTZ_FREQUENCY, 500UL * 1000UL);
     settings.mRequestedMode = ACAN2515Settings::NormalMode;
 
@@ -102,6 +106,24 @@ void setup()
     {
         Serial.print("Configuration error: 0x");
         Serial.println(errorCode, HEX);
+    }
+
+    // Configure ACAN2515
+    Serial.println("Configuring ACAN2515 CAN2...");
+    ACAN2515Settings settings2(QUARTZ_FREQUENCY, 500UL * 1000UL);
+    settings2.mRequestedMode = ACAN2515Settings::NormalMode;
+
+    const uint16_t errorCode2 = can2.begin(settings2, []
+                                           { can2.isr(); });
+
+    if (errorCode2 == 0)
+    {
+        Serial.println("CAN Initialized Successfully!");
+    }
+    else
+    {
+        Serial.print("Configuration error: 0x");
+        Serial.println(errorCode2, HEX);
     }
 }
 
@@ -136,6 +158,22 @@ void sendCANMessage()
 
     const bool ok = can.tryToSend(frame);
     if (ok)
+    {
+        gSentFrameCount += 1;
+        Serial.print("Sent: ");
+        Serial.println(gSentFrameCount);
+        Serial.println(can.transmitBufferCount(0));
+
+        Serial.print("Transmit error");
+        Serial.println(can.errorFlagRegister(), BIN);
+    }
+    else
+    {
+        Serial.println("Send failure");
+    }
+
+    const bool ok2 = can2.tryToSend(frame);
+    if (ok2)
     {
         gSentFrameCount += 1;
         Serial.print("Sent: ");
